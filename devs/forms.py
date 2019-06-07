f32f45e5fb07378fe13b9ea4bdc246e02bea45ef
sirrotich <rotichtitus12@gmail.com>
2019-06-07 11:25:12 +0300

    track user autorization

--- devs/forms.py
+++ devs/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post, Profile, Comments
 from pyuploadcare.dj.forms import FileWidget
 from pyuploadcare.dj.models import ImageField
 
