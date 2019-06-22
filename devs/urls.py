f225e528ed96930b34d4b1b024f37064ac458178
sirrotich <rotichtitus12@gmail.com>
2019-06-22 20:45:21 +0300

    url for swjs

--- devs/urls.py
+++ devs/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^devs', views.devs, name='devs'),
